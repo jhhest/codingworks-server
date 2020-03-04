22ac55a8c8bb5324ef7b91030ccae5a6201b8615
jhhest <janvanhest@outlook.com>
2020-03-04 12:01:36 +0100

    Added db.js to be able to initialize a database connection if needed

---
+++ db.js
@@ -0,0 +1,13 @@
+const Sequelize = require("sequelize");
+
+const databaseUrl =
+  process.env.DATABASE_URL ||
+  "postgres://postgres:password@localhost:5432/postgres";
+
+const db = new Sequelize(databaseUrl);
+
+db.sync({ force: false }).then(() => {
+  console.log(`\n---\n Database is connected \n---\n`);
+});
+
+module.exports = db;
