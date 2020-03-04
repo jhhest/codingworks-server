fe9ba032ec83d7d9f481e164c3ba40e7db18623b
jhhest <janvanhest@outlook.com>
2020-03-04 12:10:52 +0100

    added model/entity for JobApplicant

---
+++ models/jobApplicant.js
@@ -0,0 +1,21 @@
+const db = require("../../db");
+const Sequelize = require("sequelize");
+const JobApplicant = db.define("job applicant", {
+  fullName: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  dateOfBirth: {
+    type: Sequelize.DATEONLY,
+    allowNull: false
+  },
+  mail: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  adress: {
+    type: Sequelize.TEXT,
+    allowNull: false
+  }
+});
+module.exports = JobApplicant;
