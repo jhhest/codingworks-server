efe1aa75ffead2ee976c450e8b91ec1461043e2d
jhhest <janvanhest@outlook.com>
2020-03-04 12:14:48 +0100

    added basic functionality for jobapplicant route.

---
+++ routes/jobApplicant.js
@@ -0,0 +1,16 @@
+
+const { Router } = require("express");
+const JobApplicant = require("../models/");
+
+const router = new Router();
+
+router.post("/test", async (request, response, next) => {
+  try {
+    console.log("\n---\ntest\n---\n");
+    response.send("This is a test");
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;
