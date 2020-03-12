9a64417cd1732e2814871141a515cfd666348516
jhhest <janvanhest@outlook.com>
2020-03-12 21:53:31 +0100

    Updated authentication router. Implemented user model in authentication router

--- routes/authentication.js
+++ routes/authentication.js
@@ -1,36 +1,41 @@
 const { Router } = require("express");
 const bcrypt = require("bcrypt");
+
 const { toJWT } = require("../authentication/jwt");
 
-const JobApplicantAccount = require("../models/jobApplicantAccount");
+const User = require("../models/user")
 
 const router = new Router();
 
 router.post("/login", async (request, response, next) => {
   try {
-    const { mail, password } = request.body; // FIXME: request.body || request.query
-    console.log("mail and password are", mail, " ", password);
+    const { email, password } = request.body; // FIXME: request.body || request.query
+    console.log("mail and password are", email, " ", password);
 
-    if (!mail || !password) {
+    if (!email || !password) {
       response.status(400).send({
         message: "Not a valid email and password combination"
       });
     }
-    const jobApplicant = await JobApplicantAccount.findOne({ where: { mail } });
-    console.log("value of jobApplicant", jobApplicant);
-    if (!jobApplicant) {
+    const user = await User.findOne({ where: { email } });
+    console.log("value of user", user);
+    if (!user) {
       response.status(404).send("User is not found");
     } else {
       const isValidPassword = bcrypt.compareSync(
         password,
-        jobApplicant.password
+        user.password
       );
       if (isValidPassword) {
         response.send({
-          fullName: jobApplicant.fullName,
-          mail: jobApplicant.mail,
-          jwt: toJWT({ jobApplicantId: jobApplicant.id }),
-          jobApplicantId: jobApplicant.id
+          firstName: user.firstName,
+          lastName: user.lastName,
+          telephoneNumber: user.telephoneNumber,
+          email: user.email,
+          dateOfBirth: user.dateOfBirth,
+          city: user.city,
+          jwt: toJWT({ userId: user.id }),
+          userId: user.id
         });
       } else {
         response.status(400).send({
